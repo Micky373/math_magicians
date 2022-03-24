27d89ff0f20c07060b16e714c15620715454a997
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-24 12:54:36 +0300

    Finalize the react_events branch

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,37 +1,102 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { total: 0, operation: '', next: '' };
   }
 
+  handleClick = (items) => this.setState((e) => calculate(e, items.target.textContent));
+
   render() {
+    const { total, operation, next } = this.state;
+    const display = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : '0'} `;
     return (
       <div className="calculator_container">
-        <div className="display">
-          <h1>0</h1>
-        </div>
+        <div className="display"><span className="display_string">{display}</span></div>
         <div className="button_container">
-          <button type="button" className="button">AC</button>
-          <button type="button" className="button">+/-</button>
-          <button type="button" className="button">%</button>
-          <button type="button" className="button operator">/</button>
-          <button type="button" className="button">7</button>
-          <button type="button" className="button">8</button>
-          <button type="button" className="button">9</button>
-          <button type="button" className="button operator">x</button>
-          <button type="button" className="button">4</button>
-          <button type="button" className="button">5</button>
-          <button type="button" className="button">6</button>
-          <button type="button" className="button operator">-</button>
-          <button type="button" className="button">1</button>
-          <button type="button" className="button">2</button>
-          <button type="button" className="button">3</button>
-          <button type="button" className="button operator">+</button>
-          <button type="button" className="button item0">0</button>
-          <button type="button" className="button">.</button>
-          <button type="button" className="button operator">=</button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            AC
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            +/-
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            %
+          </button>
+          <button
+            type="button"
+            className="button operator"
+            onClick={this.handleClick}
+          >
+            ÷
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            7
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            8
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            9
+          </button>
+          <button
+            type="button"
+            className="button operator"
+            onClick={this.handleClick}
+          >
+            x
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            4
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            5
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            6
+          </button>
+          <button
+            type="button"
+            className="button operator"
+            onClick={this.handleClick}
+          >
+            -
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            1
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            2
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            3
+          </button>
+          <button
+            type="button"
+            className="button operator"
+            onClick={this.handleClick}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="button item0"
+            onClick={this.handleClick}
+          >
+            0
+          </button>
+          <button type="button" className="button" onClick={this.handleClick}>
+            .
+          </button>
+          <button
+            type="button"
+            className="button operator"
+            onClick={this.handleClick}
+          >
+            =
+          </button>
         </div>
       </div>
     );
