074eb91e01e6f06a0dd511442ba9d56d80d11a5e
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-24 13:45:41 +0300

    Some problem occured

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,38 +1,22 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { total: 0, operation: '', next: '' };
   }
 
+  handleClick = (items) => this.setState((e) => calculate(e, items.target.textContent));
+
   render() {
+    const { total, operation, next } = this.state;
+    const display = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : '0'} `;
+    const buttonsArray = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='].map((keys) => <button type="button" className="button" id={keys)} key={keys} onClick={this.handleClick}>{keys}</button>);
     return (
       <div className="calculator_container">
-        <div className="display">
-          <h1>0</h1>
-        </div>
-        <div className="button_container">
-          <button type="button" className="button">AC</button>
-          <button type="button" className="button">+/-</button>
-          <button type="button" className="button">%</button>
-          <button type="button" className="button operator">/</button>
-          <button type="button" className="button">7</button>
-          <button type="button" className="button">8</button>
-          <button type="button" className="button">9</button>
-          <button type="button" className="button operator">x</button>
-          <button type="button" className="button">4</button>
-          <button type="button" className="button">5</button>
-          <button type="button" className="button">6</button>
-          <button type="button" className="button operator">-</button>
-          <button type="button" className="button">1</button>
-          <button type="button" className="button">2</button>
-          <button type="button" className="button">3</button>
-          <button type="button" className="button operator">+</button>
-          <button type="button" className="button item0">0</button>
-          <button type="button" className="button">.</button>
-          <button type="button" className="button operator">=</button>
-        </div>
+        <div className="display"><div className="display_string">{display}</div></div>
+        <div className="button_container">{buttonsArray}</div>
       </div>
     );
   }
